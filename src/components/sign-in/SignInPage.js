74c41bc53936e059c46a6b71c59c5a35e924c3b3
Mantas Petrauskas <mantasp147@gmail.com>
2023-03-13 00:01:33 +0200

    Rebasing

--- src/components/sign-in/SignInPage.js
+++ src/components/sign-in/SignInPage.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { loginToFirebase } from "../../utils/firebase/user";
 
 import {useDispatch} from 'react-redux';
 
@@ -11,6 +13,7 @@ import {
   OutlinedButton,
   DescriptionText,
   Bubble,
+  ErrorText,
 } from "../../utils/styles/styles";
 
 import { ReactComponent as SittingWomen } from "../../svgs/sitting.svg";
@@ -20,6 +23,49 @@ import Input from "../input/Input";
 import Anchor from "../nav-achor/Anchor";
 
 function SignInPage() {
+  const [loginInformation, setLoginInformation] = useState({
+    email: "",
+    password: "",
+  });
+  const [errors, setErrors] = useState({});
+
+  const onChange = (e, name) => {
+    const { value } = e.target;
+
+    setLoginInformation((prevState) => ({ ...prevState, [`${name}`]: value }));
+  };
+
+  const validateInputs = () => {
+    const { email, password } = loginInformation;
+    setErrors({});
+    let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (password.length === 0) {
+      setErrors((prevState) => ({
+        ...prevState,
+        password: "Password is required.",
+      }));
+    }
+    if (email.length === 0) {
+      setErrors((prevState) => ({
+        ...prevState,
+        email: "Email is required.",
+      }));
+    } else if (!email.match(mailformat)) {
+      setErrors((prevState) => ({
+        ...prevState,
+        email: "Email doesn't meet pattern.",
+      }));
+    }
+    if (Object.keys(errors).length > 0) return false;
+    return true;
+  };
+
+  const handleLogin = () => {
+    if (validateInputs()) {
+      loginToFirebase(loginInformation).then((user) => console.log(user));
+    }
+  };
+
   return (
     <LayoutContainer>
       <Container>
@@ -28,11 +74,23 @@ function SignInPage() {
             <HeaderText>Welcome Back!</HeaderText>
             <OutlinedButton>Sign in with Google</OutlinedButton>
             <DescriptionText style={{ margin: "0 auto" }}>or</DescriptionText>
-            <Input label='Email' placeholder='Enter your email' />
-            <Input label='Password' placeholder='Enter your password' />
-            <PrimaryButton>Login</PrimaryButton>
+            <Input
+              type='text'
+              label='Email'
+              onChange={(e) => onChange(e, "email")}
+              placeholder='Enter your email'
+            />
+            <ErrorText>{errors.email}</ErrorText>
+            <Input
+              type='password'
+              label='Password'
+              onChange={(e) => onChange(e, "password")}
+              placeholder='Enter your password'
+            />
+            <ErrorText>{errors.password}</ErrorText>
+            <PrimaryButton onClick={handleLogin}>Login</PrimaryButton>
             <DescriptionText>
-              Still not our member? <Anchor href="/register">Sign up</Anchor>
+              Still not our member? <Anchor href='/register'>Sign up</Anchor>
             </DescriptionText>
           </TextContainer>
           <Bubble>
