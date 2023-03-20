0755c8e36cf2a45ff3c143c660ae14e0f2eb4031
markli <markasklimovas@gmail.com>
2023-03-20 20:21:45 +0200

    Add route to individual page of restaurant

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import SignInPage from "./components/sign-in/SignInPage";
 import RegisterPage from "./components/register/RegisterPage";
 import RestaurantPrompts from "./components/restaurant-info/RestaurantPrompts";
 
+
 import { Routes, Route } from "react-router-dom";
 
 import { auth } from "./firebase";
@@ -15,6 +16,8 @@ import { loginUserSuccess } from "./redux/reducers/user/user.actions";
 
 import { useSelector, useDispatch } from "react-redux";
 import { selectUserAuthentication } from "./redux/reducers/user/user.selectors";
+import IndividualRestaurant from "./components/restaurant/Restaurant";
+import SpecificRestaurant from "./components/restaurant/individual/IndividualRestaurantMenu" // rename 
 
 function App() {
   const isAuthenticated = useSelector(selectUserAuthentication);
@@ -37,6 +40,8 @@ function App() {
         <Route path='/' element={<Homepage />}></Route>
         <Route path='/restaurants' element={<RestaurantsPage />} />
         <Route path='/restaurants/:id' element={<RestaurantPrompts />} />
+        <Route path='/individual/' element={<SpecificRestaurant/>}></Route>
+
         {!isAuthenticated && (
           <Route path='/sign-in' element={<SignInPage />}></Route>
         )}
