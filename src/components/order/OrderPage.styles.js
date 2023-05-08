a42bd760cd123aeb8b36fcb1d469bd9a0ff39670
markli <markasklimovas@gmail.com>
2023-05-08 12:58:11 +0300

    Change position of order page popup menu

--- src/components/order/OrderPage.styles.js
+++ src/components/order/OrderPage.styles.js
@@ -10,7 +10,7 @@ import {
 export const Container = styled.div`
   width: 100vw;
   height: 100vh;
-  position: absolute;
+  position: fixed;
   top: 0;
   right: 0;
   background-color: rgba(0, 0, 0, 0.5);
