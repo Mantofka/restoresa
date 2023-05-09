83dc5e87992738754c17caeb112ecd903f30c307
Nojus Stasiunas <84374792+nojussta@users.noreply.github.com>
2023-05-09 20:44:31 +0300

    add/firebaseendpoint

--- src/config.js
+++ src/config.js
@@ -2,6 +2,7 @@ const Config = {
   StripePublishableKey:
     "pk_test_51MxUGcAW162dDEIYGgIi6Y7fVx1U9TBWJfcTWnkGbEJ6ueEyzI6WV7PgRCM9fPvAN2Diy20xs1RIkqgdTpbhjkhV00BCAgY7uq",
   BackendEndPoint: "https://restoresa-backend.herokuapp.com/api",
+  FirebaseEndPoint: "https://us-central1-restoresa-65368.cloudfunctions.net",
   ImageKitEndpoint: "https://ik.imagekit.io/zutjhmxfg/",
 };
 
